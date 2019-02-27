749b52f6aa7b42020d8b8a986658c069a7c42ef4
owner <themichellemcguire@gmail.com>
2019-02-27 14:08:33 -0600

    commented out dotenv

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var session = require('express-session');
 var passport = require('passport');
 var logger = require('morgan');
 
-require('dotenv').config();
+// require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 
